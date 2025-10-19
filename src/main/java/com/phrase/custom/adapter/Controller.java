38c16355da297c159d9440a5f7a21f8381d966e1
petr.zemanek <petr.zemanek@phrase.com>
2025-10-19 13:50:11 +0200

    logging

--- src/main/java/com/phrase/custom/adapter/Controller.java
+++ src/main/java/com/phrase/custom/adapter/Controller.java
@@ -8,6 +8,9 @@ import com.phrase.custom.adapter.dto.response.ErrorResponse;
 import com.phrase.custom.adapter.dto.response.LanguagesResponse;
 import com.phrase.custom.adapter.dto.response.StatusResponse;
 import com.phrase.custom.adapter.dto.response.TranslateResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static java.util.Collections.list;
+import static java.util.stream.Collectors.joining;
 import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class Controller {
 
+    Logger logger = LoggerFactory.getLogger(Controller.class);
+
     @PostMapping("/languages")
-    public ResponseEntity<LanguagesResponse> languages(@RequestBody LanguagesRequest languagesRequest) {
+    public ResponseEntity<LanguagesResponse> languages(@RequestBody LanguagesRequest languagesRequest, HttpServletRequest request) {
+        logger.info("Languages request: {}", languagesRequest);
+        processHeaders(request);
+
         LanguagesResponse languagesResponse = new LanguagesResponse(List.of(
                 new LanguagesResponse.LanguagePair(new Locale("en"), new Locale("de")),
                 new LanguagesResponse.LanguagePair(new Locale("en"), new Locale("cs"))
@@ -33,14 +43,20 @@ public class Controller {
     }
 
     @PostMapping("/status")
-    public ResponseEntity<StatusResponse> status(@RequestBody StatusRequest statusRequest) {
+    public ResponseEntity<StatusResponse> status(@RequestBody StatusRequest statusRequest, HttpServletRequest request) {
+        logger.info("Status request: {}", statusRequest);
+        processHeaders(request);
+
         StatusResponse statusResponse = new StatusResponse(StatusResponse.Status.OK);
 
         return new ResponseEntity<>(statusResponse, OK);
     }
 
     @PostMapping("/translate")
-    public ResponseEntity<TranslateResponse> translate(@RequestBody TranslateRequest translateRequest) {
+    public ResponseEntity<TranslateResponse> translate(@RequestBody TranslateRequest translateRequest, HttpServletRequest request) {
+        logger.info("Translate request: {}", translateRequest);
+        processHeaders(request);
+
         List<TranslateResponse.TranslatedSegment> translatedSegments = translateRequest.segments().stream()
                 .map(s -> new TranslateResponse.TranslatedSegment(
                                 s.idx(),
@@ -59,6 +75,18 @@ public class Controller {
         );
 
         return new ResponseEntity<>(translateResponse, OK);
+    }
+
+    public void processHeaders(HttpServletRequest request) {
+        String headers = list(request.getHeaderNames())
+                .stream()
+                .flatMap(headerName ->
+                        list(request.getHeaders(headerName))
+                                .stream()
+                                .map(headerValue -> "%s: %s".formatted(headerName, headerValue))
+                ).collect(joining(", "));
+
+        logger.info("Http headers: {}", headers);
     }
 
     @ExceptionHandler(Exception.class)
